5f12b4f1748de3bd963982c05c71ed665991a9f3
Sergio8800 <wasp8800@gmail.com>
2024-03-13 00:34:54 +0100

    class directory

--- 06_logging/home_06_catalog.py
+++ 06_logging/home_06_catalog.py
@@ -2,45 +2,51 @@ import logging
 import os
 from collections import namedtuple
 
-logging.basicConfig(filename='Data_log.log',
-                    filemode='w',
-                    encoding='utf-8',
-                    format='{levelname} - {asctime} : {msg}',
-                    style='{',
-                    level=logging.INFO)
+class Catalog_separe:
 
-logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='log-catalog.log',
+                        filemode='w',
+                        encoding='utf-8',
+                        format='{levelname} >>> - {msg} >>> : {asctime} ',
+                        style='{',
+                        level=logging.INFO)
 
-data_list = [(dirs, folders, files) for dirs, folders, files in
-             os.walk('C:\_python\LEETCODE')]
-clas_list = []
+    logger = logging.getLogger(__name__)
 
-Data = namedtuple('Data',
-                  ['file_name', 'file_exten', 'dir_flag', 'parent_dir'])
-for i in range(0, len(data_list)):
-    parent_dir = data_list[i][0]
-    dir_list = data_list[i][1]
-    file_list = data_list[i][2]
+    def __init__(self, dir='C:\_python\LEETCODE'):
+        self.directory = dir
 
-    for el in dir_list:
-        dir_flag = 'Yes'
-        file_name = el
-        file_exten = ''
-        d = Data(file_name, file_exten, dir_flag, parent_dir)
-        clas_list.append(d)
-        logger.info(
-            f'{file_name}, {file_exten}, {dir_flag}, {parent_dir}')
+    def separate(self):
+        data_list = [(dirs, folders, files) for dirs, folders, files in
+                     os.walk(self.directory)]
+        clas_list = []
 
-    for item in file_list:
-        dir_flag = 'No'
-        try:
-            file_name, file_exten = item.split('.')
-        except Exception:
-            *file_name, file_exten = item.split('.')
+        Data = namedtuple('Data',
+                          ['file_name', 'file_exten', 'dir_flag', 'parent_dir'])
+        for i in range(0, len(data_list)):
+            parent_dir = data_list[i][0]
+            dir_list = data_list[i][1]
+            file_list = data_list[i][2]
 
-        d = Data(file_name, file_exten, dir_flag, parent_dir)
-        clas_list.append(d)
-        logger.info(
-            f'{file_name}, {file_exten}, {dir_flag}, {parent_dir}')
+            for el in dir_list:
+                dir_flag = 'Yes'
+                file_name = el
+                file_exten = ''
+                d = Data(file_name, file_exten, dir_flag, parent_dir)
+                clas_list.append(d)
+                self.logger.info(f'{file_name}, {file_exten}, {dir_flag}, {parent_dir}')
 
-print(*clas_list, sep="\n")
+            for item in file_list:
+                dir_flag = 'No'
+                try:
+                    file_name, file_exten = item.split('.')
+                except Exception:
+                    *file_name, file_exten = item.split('.')
+
+                d = Data(file_name, file_exten, dir_flag, parent_dir)
+                clas_list.append(d)
+                self.logger.info(
+                    f'{file_name}, {file_exten}, {dir_flag}, {parent_dir}')
+                return print(*clas_list, sep="\n")
+
+Catalog_separe().separate()
